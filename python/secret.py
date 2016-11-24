9e94fce4261990c9cf74ff53144682667c4f3087
AntoineB <antoine.borie@eu.univ-orleans.fr>
2016-11-24 11:56:44 +0100

    Mise en place du projet

---
+++ python/secret.py
@@ -0,0 +1,13 @@
+import base64
+
+from Crypto.Cipher import DES
+
+
+def pad(s): return s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
+
+
+msg = "The cake is a lie!\n"
+key = "1337DEADBEEF1664".decode('hex')
+iv = "0102030405060708".decode('hex')
+cipher = DES.new(key, DES.MODE_CBC, iv)
+print base64.encodestring(cipher.encrypt(pad(msg)))
